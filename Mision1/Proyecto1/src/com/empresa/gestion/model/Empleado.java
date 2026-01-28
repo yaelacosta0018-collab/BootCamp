b0e2ea3ede311a051497feb26912c397f50fa3d9
yaelacosta0018-collab <yaelacosta0018@gmail.com>
2026-01-27 21:52:08 -0500

    Creacion de clase Empleado,Gerente,Persona

---
+++ Mision1/Proyecto1/src/com/empresa/gestion/model/Empleado.java
@@ -0,0 +1,5 @@
+package com.empresa.gestion.model;
+
+public class Empleado {
+    
+}
---
+++ Mision1/Proyecto1/src/com/empresa/gestion/model/Persona.java
@@ -0,0 +1,19 @@
+package com.empresa.gestion.model;
+
+public class Persona {
+    // Encapsulación
+  private String nombre;
+  private int edad;
+ 
+   // Constructor
+  public Persona(String nombre, int edad){
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+  public String getNombre(){
+    return nombre;
+  }
+  public void setNombre(String nombre){
+    this.nombre = nombre;
+  }
+}
